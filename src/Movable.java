336defb3798ac57c2d2a2397d015733f4d7a7aeb
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2024-12-09 23:02:29 -0600

    Interface

--- src/Movable.java
+++ src/Movable.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public interface Movable {
+    // An interface defines a list of public abstract methods to be implemented by the subclasses
+
+    void moveUp();    // "public" and "abstract" by default
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+    String getCoordinate();
+
 }
